bb3646f729f5476224cb6ff6ac0c19c66d397d85
jean <christophewizamo@gmail.com>
2023-02-19 18:49:29 +0100

    about and values

--- siteKim/resources/views/includes/carousel.blade.php
+++ siteKim/resources/views/includes/carousel.blade.php
@@ -7,8 +7,10 @@
                         <h2 class="slide-title" data-animation-in="slideInLeft">17 Years of excellence in</h2>
                         <h3 class="slide-sub-title" data-animation-in="slideInRight">Construction Industry</h3>
                         <p data-animation-in="slideInLeft" data-duration-in="1.2">
-                            <a href="services.html" class="slider btn btn-primary" style="background-color: #03224c;">Our Services</a>
-                            <a href="contact.html" class="slider btn btn-primary border" style="background-color: #03224c;">Contact Now</a>
+                            <a href="services.html" class="slider btn btn-primary"
+                                style="background-color: #03224c;">Our Services</a>
+                            <a href="contact.html" class="slider btn btn-primary border"
+                                style="background-color: #03224c;">Contact Now</a>
                         </p>
                     </div>
                 </div>
@@ -21,11 +23,13 @@
             <div class="container h-100">
                 <div class="row align-items-center h-100">
                     <div class="col-md-12">
-                        <h2 class="slide-title-box" data-animation-in="slideInDown" style="background-color: #03224c;">World Class Service</h2>
+                        <h2 class="slide-title-box" data-animation-in="slideInDown" style="background-color: #03224c;">
+                            World Class Service</h2>
                         <h3 class="slide-title" data-animation-in="fadeIn">When Service Matters</h3>
                         <h3 class="slide-sub-title" data-animation-in="slideInLeft">Your Choice is Simple</h3>
                         <p data-animation-in="slideInRight">
-                            <a href="services.html" class="slider btn btn-primary border" style="background-color: #03224c;">Our Services</a>
+                            <a href="services.html" class="slider btn btn-primary border"
+                                style="background-color: #03224c;">Our Services</a>
                         </p>
                     </div>
                 </div>
@@ -43,10 +47,11 @@
                         <p class="slider-description lead" data-animation-in="slideInRight">We will deal with your
                             failure that determines how you achieve success.</p>
                         <div data-animation-in="slideInLeft">
-                            <a href="contact.html" class="slider btn btn-primary" aria-label="contact-with-us" style="background-color: #03224c;">Get Free
+                            <a href="contact.html" class="slider btn btn-primary" aria-label="contact-with-us"
+                                style="background-color: #03224c;">Get Free
                                 Quote</a>
-                            <a href="about.html" class="slider btn btn-primary border"
-                                aria-label="learn-more-about-us" style="background-color: #03224c;">Learn more</a>
+                            <a href="about.html" class="slider btn btn-primary border" aria-label="learn-more-about-us"
+                                style="background-color: #03224c;">Learn more</a>
                         </div>
                     </div>
                 </div>
@@ -60,7 +65,7 @@
         <div class="action-style-box" style="background-color: #03224c;">
             <div class="row align-items-center">
                 <div class="col-md-8 text-center text-md-left" style="background-color: #03224c;">
-                    <div class="call-to-action-text" >
+                    <div class="call-to-action-text">
                         <h3 class="action-title">We understand your needs on construction</h3>
                     </div>
                 </div><!-- Col end -->
@@ -80,12 +85,11 @@
             <div class="col-lg-6">
                 <div class="ts-intro">
                     <h2 class="into-title">About Us</h2>
-                    <h3 class="into-sub-title">We deliver landmark projects</h3>
-                    <p>We are rethoric question ran over her cheek When she reached the first hills of the Italic
-                        Mountains,
-                        she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet
-                        Village
-                        and the subline of her own road, the Line Lane.</p>
+                    <h3 class="into-sub-title">NOUS LIVRONS DES PROJETS PHARES</h3>
+                    <p>MSDI fournit des solutions UAV pour l'industrie et la topographie en Indonésie. Cartographie par
+                        drone, technologie LiDAR haut de gamme, haute précision pour projet immobilier ou de génie
+                        civil, gestion forestière, pétrole et gaz, corridor, exploitation minière, modèle 3D,
+                        archéologie, inspections d'actifs.</p>
                 </div><!-- Intro box end -->
 
                 <div class="gap-20"></div>
@@ -94,10 +98,12 @@
                     <div class="col-md-6">
                         <div class="ts-service-box">
                             <span class="ts-service-icon">
-                                <i class="fas fa-trophy"></i>
+                                <i class="fas fa-graduation-cap"></i>
                             </span>
                             <div class="ts-service-box-content">
-                                <h3 class="service-box-title">We've Repution for Excellence</h3>
+                                <h3 class="service-box-title">Pilotes brevetés</h3>
+                                <p>Dans notre travail, les drones ne sont pas des jouets, c'est pourquoi tous nos
+                                    opérateurs de drones sont agréés par le ministère indonésien des transports.</p>
                             </div>
                         </div><!-- Service 1 end -->
                     </div><!-- col end -->
@@ -105,10 +111,12 @@
                     <div class="col-md-6">
                         <div class="ts-service-box">
                             <span class="ts-service-icon">
-                                <i class="fas fa-sliders-h"></i>
+                                <i class="fas fa-sticky-note"></i>
                             </span>
                             <div class="ts-service-box-content">
-                                <h3 class="service-box-title">We Build Partnerships</h3>
+                                <h3 class="service-box-title">Assurance</h3>
+                                <p>Chaque projet est différent et peut contenir des surprises, nos drones sont couverts
+                                    par une assurance internationale.</p>
                             </div>
                         </div><!-- Service 2 end -->
                     </div><!-- col end -->
@@ -118,10 +126,12 @@
                     <div class="col-md-6">
                         <div class="ts-service-box">
                             <span class="ts-service-icon">
-                                <i class="fas fa-thumbs-up"></i>
+                                <i class="fas fa-cog"></i>
                             </span>
                             <div class="ts-service-box-content">
-                                <h3 class="service-box-title">Guided by Commitment</h3>
+                                <h3 class="service-box-title">Ingénierie</h3>
+                                <p>Notre équipe est composée d'ingénieurs prêts à travailler sur votre projet et à vous
+                                    fournir les meilleurs résultats possibles.</p>
                             </div>
                         </div><!-- Service 1 end -->
                     </div><!-- col end -->
@@ -129,10 +139,13 @@
                     <div class="col-md-6">
                         <div class="ts-service-box">
                             <span class="ts-service-icon">
-                                <i class="fas fa-users"></i>
+                                <i class="fas fa-clock"></i>
                             </span>
                             <div class="ts-service-box-content">
-                                <h3 class="service-box-title">A Team of Professionals</h3>
+                                <h3 class="service-box-title">Réponse rapide et livraison rapide</h3>
+                                <p>Parce que le temps est précieux et que certains projets ne peuvent attendre, nous
+                                    vous répondons sous 24H et nous vous livrons les dossiers définitifs dans les
+                                    meilleurs délais.</p>
                             </div>
                         </div><!-- Service 2 end -->
                     </div><!-- col end -->
@@ -140,7 +153,7 @@
             </div><!-- Col end -->
 
             <div class="col-lg-6 mt-4 mt-lg-0">
-                <h3 class="into-sub-title">Our Values</h3>
+                <h3 class="into-sub-title">Nos valeurs</h3>
                 <p>Minim Austin 3 wolf moon scenester aesthetic, umami odio pariatur bitters. Pop-up occaecat taxidermy
                     street art, tattooed beard literally.</p>
 
@@ -150,7 +163,7 @@
                             <h2 class="mb-0">
                                 <button class="btn btn-block text-left" type="button" data-toggle="collapse"
                                     data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                    Safety
+                                    La Sécurité
                                 </button>
                             </h2>
                         </div>
@@ -169,7 +182,7 @@
                                 <button class="btn btn-block text-left collapsed" type="button"
                                     data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false"
                                     aria-controls="collapseTwo">
-                                    Customer Service
+                                    Le Service de clientèle
                                 </button>
                             </h2>
                         </div>
@@ -187,7 +200,7 @@
                                 <button class="btn btn-block text-left collapsed" type="button"
                                     data-toggle="collapse" data-target="#collapseThree" aria-expanded="false"
                                     aria-controls="collapseThree">
-                                    Integrity
+                                    l'Intégrité
                                 </button>
                             </h2>
                         </div>
@@ -196,6 +209,41 @@
                             <div class="card-body">
                                 Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
                                 squid. 3 wolf moon officia aute, non cupidata
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header p-0 bg-transparent" id="headingThree">
+                            <h2 class="mb-0">
+                                <button class="btn btn-block text-left collapsed" type="button"
+                                    data-toggle="collapse" data-target="#collapseThree" aria-expanded="false"
+                                    aria-controls="collapseThree">
+                                    La rigueur
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree"
+                            data-parent="#our-values-accordion">
+                            <div class="card-body">
+                                Travail avec méthode et professionnalisme pour assurer l'excellence et le respect de nos
+                                engagements
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header p-0 bg-transparent" id="headingThree">
+                            <h2 class="mb-0">
+                                <button class="btn btn-block text-left collapsed" type="button"
+                                    data-toggle="collapse" data-target="#collapseThree" aria-expanded="false"
+                                    aria-controls="collapseThree">
+                                    La passion
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree"
+                            data-parent="#our-values-accordion">
+                            <div class="card-body">
+                                Nous accomplissons notre travail avec amour et énergie
                             </div>
                         </div>
                     </div>
