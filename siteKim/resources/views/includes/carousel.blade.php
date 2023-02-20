bcf40c1c01875bccd9b1a6330c54cc9cac6df15e
jean <christophewizamo@gmail.com>
2023-02-20 10:44:54 +0100

    carousel

--- siteKim/resources/views/includes/carousel.blade.php
+++ siteKim/resources/views/includes/carousel.blade.php
@@ -1,16 +1,19 @@
 <div class="banner-carousel banner-carousel-1 mb-0">
-    <div class="banner-carousel-item" style="background-image:url({{ asset('front-end/images/slider-main/bg1.jpg') }})">
+    <div class="banner-carousel-item" style="background-image:url({{ asset('front-end/images/drone1.jpg') }})">
         <div class="slider-content">
             <div class="container h-100">
                 <div class="row align-items-center h-100">
                     <div class="col-md-12 text-center">
-                        <h2 class="slide-title" data-animation-in="slideInLeft">17 Years of excellence in</h2>
-                        <h3 class="slide-sub-title" data-animation-in="slideInRight">Construction Industry</h3>
+                        <h2 class="slide-title-box" data-animation-in="slideInLeft" style="background-color: #03224c;">CARTOGRAPHIE DE PHOTOGRAMMÉTRIE</h2>
+                        <p class="slider-description lead" data-animation-in="slideInRight">La cartographie par drone est la
+                            nouvelle solution tendance pour réaliser une carte 3D d'un terrain, des courbes de niveau,
+                            un rendu 3D de ville ou d'immeuble. La photogrammétrie est la technologie cartographique
+                            d'entrée de gamme.</p>
                         <p data-animation-in="slideInLeft" data-duration-in="1.2">
                             <a href="services.html" class="slider btn btn-primary"
-                                style="background-color: #03224c;">Our Services</a>
-                            <a href="contact.html" class="slider btn btn-primary border"
-                                style="background-color: #03224c;">Contact Now</a>
+                                style="background-color: #03224c;">Voir Plus</a>
+                            {{-- <a href="contact.html" class="slider btn btn-primary border"
+                                style="background-color: #03224c;">Contacter Maintenant</a> --}}
                         </p>
                     </div>
                 </div>
@@ -18,18 +21,21 @@
         </div>
     </div>
 
-    <div class="banner-carousel-item" style="background-image:url({{ asset('front-end/images/slider-main/bg2.jpg') }})">
+    <div class="banner-carousel-item" style="background-image:url({{ asset('front-end/images/drone2.jpg') }})">
         <div class="slider-content text-left">
             <div class="container h-100">
                 <div class="row align-items-center h-100">
                     <div class="col-md-12">
                         <h2 class="slide-title-box" data-animation-in="slideInDown" style="background-color: #03224c;">
-                            World Class Service</h2>
-                        <h3 class="slide-title" data-animation-in="fadeIn">When Service Matters</h3>
-                        <h3 class="slide-sub-title" data-animation-in="slideInLeft">Your Choice is Simple</h3>
+                            TOPOGRAPHIE DE HAUTE PRÉCISION</h2>
+                        <h3 class="slide-sub-title" data-animation-in="fadeIn">Nos équipements sont capables de fournir une
+                            précision centimétrique.</h3>
+                        <p class="slider-description lead" data-animation-in="slideInLeft">Nous utilisons la technologie RTK
+                            pour synchroniser nos drones afin de fournir des plans très précis. La précision varie de 3
+                            à 5 cm en photogrammétrie et entre 1,5 et 2,5 pour le LiDAR.</p>
                         <p data-animation-in="slideInRight">
                             <a href="services.html" class="slider btn btn-primary border"
-                                style="background-color: #03224c;">Our Services</a>
+                                style="background-color: #03224c;">Voir plus</a>
                         </p>
                     </div>
                 </div>
@@ -37,21 +43,22 @@
         </div>
     </div>
 
-    <div class="banner-carousel-item" style="background-image:url({{ asset('front-end/images/slider-main/bg3.jpg') }})">
+    <div class="banner-carousel-item" style="background-image:url({{ asset('front-end/images/drone3.jpg') }})">
         <div class="slider-content text-right">
             <div class="container h-100">
                 <div class="row align-items-center h-100">
                     <div class="col-md-12">
-                        <h2 class="slide-title" data-animation-in="slideInDown">Meet Our Engineers</h2>
-                        <h3 class="slide-sub-title" data-animation-in="fadeIn">We believe sustainability</h3>
-                        <p class="slider-description lead" data-animation-in="slideInRight">We will deal with your
-                            failure that determines how you achieve success.</p>
+                        <h2 class="slide-title-box" data-animation-in="slideInDown" style="background-color: #03224c;">SERVICES DRONE LIDAR</h2>
+                        <h3 class="slide-sub-title" data-animation-in="fadeIn">Oubliez toutes les contraintes de la
+                            cartographie drone traditionnelle.</h3>
+                        <p class="slider-description lead" data-animation-in="slideInRight">La cartographie LiDAR permet
+                            de percer la végétation et offre un haut niveau de précision : jusqu'à 5 couches de
+                            feuillage et 1 cm de précision avec le capteur LiDAR que nous utilisons.</p>
                         <div data-animation-in="slideInLeft">
                             <a href="contact.html" class="slider btn btn-primary" aria-label="contact-with-us"
-                                style="background-color: #03224c;">Get Free
-                                Quote</a>
-                            <a href="about.html" class="slider btn btn-primary border" aria-label="learn-more-about-us"
-                                style="background-color: #03224c;">Learn more</a>
+                                style="background-color: #03224c;">Voir plus</a>
+                            {{-- <a href="about.html" class="slider btn btn-primary border" aria-label="learn-more-about-us"
+                                style="background-color: #03224c;">Learn more</a> --}}
                         </div>
                     </div>
                 </div>
