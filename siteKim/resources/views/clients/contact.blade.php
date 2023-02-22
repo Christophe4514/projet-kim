028d7470faa9ece34743c90e2f9ee137deeab8cf
jean <christophewizamo@gmail.com>
2023-02-22 12:54:51 +0100

    contact views

--- siteKim/resources/views/clients/contact.blade.php
+++ siteKim/resources/views/clients/contact.blade.php
@@ -3,42 +3,10 @@
 @section('content')
     @include('includes.client-header')
 
-    @include('includes.contact')
+    @include('includes.banners.banner-contact')
 
-    <div class="bg-light">
-        <div class="container">
-            <h3 class="section-sub-title">Suivez-nous sur les réseaux sociaux pour plus d'actu</h3>
-        </div>
-        <div class="row">
-            <div class="col-lg-4 col-md-4 top-social text-center">
-                <ul class="list-unstyled">
-                    <li>
-                        <a title="Facebook" href="#" style="font-size: 2.5rem">
-                            <span class="social-icon"><i class="fab fa-facebook-f"></i></span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-lg-4 col-md-4 top-social text-center">
-                <ul class="list-unstyled">
-                    <li>
-                        <a title="Twitter" href="#" style="font-size: 2.5rem">
-                            <span class="social-icon"><i class="fab fa-twitter"></i></span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-lg-4 col-md-4 top-social text-center">
-                <ul class="list-unstyled">
-                    <li>
-                        <a title="Instagram" href="#" style="font-size: 2.5rem">
-                            <span class="social-icon"><i class="fab fa-instagram"></i></span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
+    @include('includes.contacter')
+
 
     @include('includes.client-footer')
 @endsection
