06b5b72b0b5dfe6e65b713f878962fd9c3ab7a77
jean <christophewizamo@gmail.com>
2023-03-01 20:50:04 +0100

    breadcrumb

--- siteKim/resources/views/includes/banners/banner-faq.blade.php
+++ siteKim/resources/views/includes/banners/banner-faq.blade.php
@@ -7,7 +7,7 @@
                         <h1 class="banner-title">Faq</h1>
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb justify-content-center">
-                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
                                 <li class="breadcrumb-item active" aria-current="page">Faq</li>
                             </ol>
                         </nav>
