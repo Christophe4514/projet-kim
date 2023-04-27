1cf89757e4c2f6f842200a30e73d871320b2f75a
christophe4514 <christophewizamo@gmail.com>
2023-04-27 11:09:24 +0100

    maj adress page contact

--- siteKim/resources/views/includes/contacter.blade.php
+++ siteKim/resources/views/includes/contacter.blade.php
@@ -52,8 +52,11 @@
 
         <div class="google-map">
             {{-- <div id="map" class="map" data-latitude="-4.322447" data-longitude="15.307045" --}}
-                {{-- data-marker="images/marker.png" data-marker-name="Constra"></div> --}}
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3978.530466313132!2d15.28418076585197!3d-4.310912486077977!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1a6a33652a0f99ab%3A0xf6187f06dca81856!2sArr%C3%AAt%2024%20NOVEMBRE!5e0!3m2!1sen!2scd!4v1682525794550!5m2!1sen!2scd" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            {{-- data-marker="images/marker.png" data-marker-name="Constra"></div> --}}
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3978.530466313132!2d15.28418076585197!3d-4.310912486077977!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1a6a33652a0f99ab%3A0xf6187f06dca81856!2sArr%C3%AAt%2024%20NOVEMBRE!5e0!3m2!1sen!2scd!4v1682525794550!5m2!1sen!2scd"
+                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
 
         <div class="gap-40"></div>
