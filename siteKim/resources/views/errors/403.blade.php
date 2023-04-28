d771eb9638604b46e225c73035b972868f96225a
Don Makila <makiladon14@gmail.com>
2023-04-28 15:29:48 -0800

    Pages Erreur

---
+++ siteKim/resources/views/errors/403.blade.php
@@ -0,0 +1,30 @@
+@extends('client-layouts.client')
+
+@section('title')
+403
+@endsection
+@section('content')
+<section id="main-container" class="main-container">
+    <div class="container">
+
+      <div class="row">
+
+        <div class="col-12">
+          <div class="error-page text-center">
+            <div class="error-code">
+              <h2><strong>403</strong></h2>
+            </div>
+            <div class="error-message">
+              <h3>Oops... Cette page vous est interdite!</h3>
+            </div>
+            <div class="error-body">
+              Utilisez ce bouton pour retourner à la page d'accueil du site<br>
+              <a href="{{url('/')}}" class="btn btn-primary">Back to Home Page</a>
+            </div>
+          </div>
+        </div>
+
+      </div><!-- Content row -->
+    </div><!-- Conatiner end -->
+  </section><!-- Main container end -->
+@endsection
